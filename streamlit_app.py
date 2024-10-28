ab8ebbb5c60d7e9476813fd418a9ecd506931c49
PavaS279 <118591603+PavaS279@users.noreply.github.com>
2024-10-28 12:20:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,7 @@ if ingredients_list:
     
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-        st.subheader(fruit_choosen + 'Nutrition Info')
-        fruityvice_response = requests.get("https://webhook.site/f55f66ba-20fc-48bc-840a-f88f449d3dc7" + fruit_choosen)
+        fruityvice_response = requests.get("https://webhook.site/f55f66ba-20fc-48bc-840a-f88f449d3dc7")
         # st.text(fruityvice_response.json())
        
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
